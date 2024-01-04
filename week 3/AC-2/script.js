c3d73763a7f485d41487cb85a8eb551401da91a6
Noah Sehman <noah.sehman@gmail.com>
2024-01-03 19:07:22 -0700

    second activity

---
+++ week 3/AC-2/script.js
@@ -0,0 +1,9 @@
+
+
+var personName = "Someone's Name";
+var pets = "a believable number of";
+var funFact = "I'm not good at coming up with names or values";
+
+console.log("My name is:", personName)
+console.log("I have", pets, "pets")
+console.log("Fun fact:", funFact)
