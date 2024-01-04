1db1869989955575b07783c3d9daed99a891e03a
Noah Sehman <noah.sehman@gmail.com>
2024-01-04 10:16:07 -0700

    new day new me. New code to add here also

--- week 3/AC-10/script.js
+++ week 3/AC-10/script.js
@@ -1,3 +1,23 @@
-var constellations = ["Orion", "Scorpius", "Lyra", "Ursa Major", "Ursa Minor"];
+var nconstellations = ["Orion", "Scorpius", "Lyra", "Ursa Major", "Ursa Minor"];
 var planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 var star = "polaris";
+
+//instead of this, I thought about just adding Canis Major and then sorting the array but eh whatever
+var constellations = ["Canis Major"];
+for (i = 0; i < nconstellations.length; i++) {
+    constellations.push(nconstellations[i]);
+}
+planets.pop();
+var bigStar = star.toUpperCase();
+
+//not REALLY sure what they were asking for so here's both ideas of what they could mean
+var galaxy1 = [constellations, planets];
+var galaxy2 = [];
+for (j = 0; j < (constellations.length + planets.length); j++) {
+    if (j < constellations.length) {
+        galaxy2.push(constellations[j]);
+    }
+    else {
+        galaxy2.push(planets[j - (constellations.length - 1)])
+    }
+}
