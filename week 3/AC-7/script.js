7d33135ff7cfd3faa4c645c657f706abce476ac1
Noah Sehman <noah.sehman@gmail.com>
2024-01-03 20:38:02 -0700

    anotha one lesgooo

---
+++ week 3/AC-7/script.js
@@ -0,0 +1,6 @@
+const students = ["John", "Saketh", "Cayla", "Ewan", "Rachana"]
+
+console.log(students.length)
+for (i = 0; i < students.length; i++) {
+    console.log("Great to see you,", students[i] + "!")
+}
