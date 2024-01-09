b46a14bdc31b8dc53290425fa9d503d83777f735
Noah Sehman <noah.sehman@gmail.com>
2024-01-08 22:18:32 -0700

    lame

--- week 3/MINIPROJ/script.js
+++ week 3/MINIPROJ/script.js
@@ -1,19 +1,42 @@
-
-const opts = ['r', 'p', 's'];
-const accGuess = ['rock', 'paper', 'scissors'];
+let accGuess = ['rock', 'paper', 'scissors'];
 
  function userGuess () {
-    var ui = prompt("Rock, Paper, or Scissors?!?\nThe choice is yours");
+    var ui = String(prompt("Rock, Paper, or Scissors?!?\nThe choice is yours"));
     ui = ui.toLowerCase();
-    while (!(ui in opts || ui in accGuess)) {
-        alert("Invalid input!!\nPlease input 'rock', 'paper', or 'scissors'")
-        ui = prompt("Rock, Paper, or Scissors?!?\nThe choice is yours").toLowerCase();
-    }
     return ui;
  }
- function dumbComp() {
-    cnum = Math.floor(Math.random() * 3)
- }
- function smartComp() {
 
+ function dumbComp() {
+    let cnum = Math.floor(Math.random() * 3);
+    let cotp = accGuess[cnum];
+    return cotp;
+ } 
+
+ function gamePlay(user, computer) {
+   console.log(user, computer)
+   if (user == computer) {
+      return 0;
+   }
+ }
+
+ gamePlay(user_input, computer_input);
+
+ function main() {
+   alert("Rock Paper Scissors or whatever\nstart playing already");
+   let tie = 0;
+   let win = 0;
+   let lose = 0;
+   let winner = gamePlay(userGuess(), dumbComp());
+   if (winner == 0) {
+      tie++;
+   }
+   else if (winner == 1) {
+      win++;
+   }
+   else if (winner == 2) {
+      lose ++
+   }
+   else {
+      alert("What have you done to my code?!?!?!")
+   }
  }
